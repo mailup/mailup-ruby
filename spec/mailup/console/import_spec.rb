7497f24a04b7f69e8755e58f9b3e95d202ce7209
byterussian <rocco@galluzzo.me>
2014-11-10 16:24:55 +0100

    fix tests

--- spec/mailup/console/import_spec.rb
+++ spec/mailup/console/import_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe MailUp::Console::Import do
   before(:each) { init_mailup }
 
-  %w(status send_confirmation_email).each do |method|
+  %w(status confirmation_email_id).each do |method|
     it "should have a #{method} method" do
       @mailup.console.import(1).should respond_to(method.to_sym)
     end
@@ -15,9 +15,9 @@ describe MailUp::Console::Import do
     @mailup.console.import(id).status
   end
 
-  it "should fire the correct GET request for send_confirmation_email" do
+  it "should fire the correct GET request for confirmation_email_id" do
     id = rand(100).abs
     @mailup.console.import(id).api.should_receive(:get).with("#{@mailup.console.import(id).api.path}/Import/#{id}/Sending")
-    @mailup.console.import(id).send_confirmation_email
+    @mailup.console.import(id).confirmation_email_id
   end
 end
